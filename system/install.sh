318ac69b5a05dfae63edd30a1f36826cd4236352
a-hardin <alan.e.hardin@gmail.com>
2019-08-20 12:43:34 -0500

    Adding env generation to install script, adding plex to docker-compose

--- system/install.sh
+++ system/install.sh
@@ -40,6 +40,20 @@ echo "*********************"
 sudo cp /var/www/homeserver/system/01-network-manager-all.yaml /etc/netplan/01-network-manager-all.yaml
 sudo netplan apply
 
+
+echo "*********************"
+echo "Setup env file"
+echo "*********************"
+sudo touch .env
+MYSQL_ROOT_PASSWORD=MYSQL_ROOT_PASSWORD=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 20 | head -n 1)
+MYSQL_PASSWORD=MYSQL_PASSWORD=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 20 | head -n 1)
+MYSQL_DATABASE=MYSQL_DATABASE=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 20 | head -n 1)
+MYSQL_USER=MYSQL_USER=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 20 | head -n 1)
+echo $MYSQL_ROOT_PASSWORD | sudo tee -a .env
+echo $MYSQL_PASSWORD | sudo tee -a .env
+echo $MYSQL_DATABASE | sudo tee -a .env
+echo $MYSQL_USER | sudo tee -a .env
+
 echo "*********************"
 echo "start docker container"
 echo "*********************"
