ab0428b0f34576cb378e186f2651c2cd11d27ce0
a-hardin <alan.e.hardin@gmail.com>
2024-10-14 14:45:05 -0500

    Adding MYSQL_GUACAMOLE_DATABASE for install

--- system/install.sh
+++ system/install.sh
@@ -92,10 +92,12 @@ MYSQL_ROOT_PASSWORD=MYSQL_ROOT_PASSWORD=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' 
 MYSQL_PASSWORD=MYSQL_PASSWORD=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 20 | head -n 1)
 MYSQL_DATABASE=MYSQL_DATABASE=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 20 | head -n 1)
 MYSQL_USER=MYSQL_USER=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 20 | head -n 1)
+MYSQL_GUACAMOLE_DATABASE=MYSQL_GUACAMOLE_DATABASE=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 20 | head -n 1)
 echo $MYSQL_ROOT_PASSWORD | sudo tee -a .env
 echo $MYSQL_PASSWORD | sudo tee -a .env
 echo $MYSQL_DATABASE | sudo tee -a .env
 echo $MYSQL_USER | sudo tee -a .env
+echo $MYSQL_GUACAMOLE_DATABASE | sudo tee -a .env
 
 echo "*********************"
 echo "start docker container"
